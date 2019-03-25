dde528d1b9c86a962d0ac0debf3cc6a41c745bc1
jocourbis <mcourbisd@udd.cl>
2019-03-24 23:13:22 -0300

    tamos casiii

--- Tarea1/build/source/Tarea1.java
+++ Tarea1/build/source/Tarea1.java
@@ -25,6 +25,7 @@ int w, tamX;
 int r = 29;
 int g = 10;
 int b = 255;
+int velocidad = 1;
 
 boolean a = false;
 
@@ -39,8 +40,10 @@ public void draw()
 {
 println(tamX,h);
 
-h++;
-w++;
+
+h = h + velocidad;
+w = w + velocidad;
+
 
 if (h > 200){
   h = 0;
@@ -88,65 +91,15 @@ if (a == true){
   tamY = tamY+1;
 
 }
-/*
 
-ellipse(width/2 + cos(n) * 100, height/2 + sin(n) * 100, 20, 20);
-  n = n + 0.5;
-
-*/
-
-/*
-
-translate(width/2, height/2);                    // punto (0,0)/ ORIGEN en el centro del sketch
-for(int i = 0; i < 842; i+=50){
-                // For que define: angulo máximo de rotación, cantidad de figuras y su separación
-    float x = sin(radians(i))*10;               // variable de rotacion
-    float y = cos(radians(i))*10;               // variable de rotacion
-    pushMatrix();                                // Abriendo propiedades independientes
-    translate(x, y);                             // Se traslada el origen, que al mismo tiempo esta en movimiento
-    rotate(radians(-i+frameCount));              // proporciona movimiento al sketch/ cambia los ejes/ direccion de rotacion
-    fill(random(255));      // Se rellena la figura con el color original, pero es alterado en el canal R. (P.127) esto hara que palpite el anillo
-    ellipse(0,0,10,10);                     // Se dibujan las figuras que se encuentran en el fondo
-    popMatrix();                                 // Cerrando propiedades independientes
 }
 
-   for(int i = 0; i < 360; i+=15.5){                 // For que define: angulo máximo de rotación, cantidad de figuras y su separación
-       float x = sin(radians(i))*50;               // variable de rotacion
-       float y = cos(radians(i))*50;               // variable de rotacion
-       pushMatrix();                                // Abriendo propiedades independientes
-       translate(x, y);                             // Se traslada el origen, que al mismo tiempo esta en movimiento
-       rotate(radians(-i-frameCount+90));              // proporciona movimiento al sketch/ cambia los ejes/ direccion de rotacion
-       fill(random(150),random(255), random(50));      // Se rellena la figura con el color original, pero es alterado en el canal R. (P.127) esto hara que palpite el anillo
-       ellipse(0,0,20,20);                     // Se dibujan las figuras que se encuentran en el fondo
-       popMatrix();                                 // Cerrando propiedades independientes
-}
-
-for(int i = 0; i < 360; i+=8.5){                 // For que define: angulo máximo de rotación, cantidad de figuras y su separación
-    float x = sin(radians(i))*100;               // variable de rotacion
-    float y = cos(radians(i))*100;               // variable de rotacion
-    pushMatrix();                                // Abriendo propiedades independientes
-    translate(x, y);                             // Se traslada el origen, que al mismo tiempo esta en movimiento
-    rotate(radians(-i-frameCount+90));              // proporciona movimiento al sketch/ cambia los ejes/ direccion de rotacion
-    fill(random(255),random(50), random(150));      // Se rellena la figura con el color original, pero es alterado en el canal R. (P.127) esto hara que palpite el anillo
-    ellipse(0,0,20,20);                     // Se dibujan las figuras que se encuentran en el fondo
-    popMatrix();                                 // Cerrando propiedades independientes
-}
-
-for(int i = 0; i < 360; i+=5.5){                 // For que define: angulo máximo de rotación, cantidad de figuras y su separación
-    float x = sin(radians(i))*150;               // variable de rotacion
-    float y = cos(radians(i))*150;               // variable de rotacion
-    pushMatrix();                                // Abriendo propiedades independientes
-    translate(x, y);                             // Se traslada el origen, que al mismo tiempo esta en movimiento
-    rotate(radians(-i-frameCount+90));              // proporciona movimiento al sketch/ cambia los ejes/ direccion de rotacion
-    fill(random(50),random(150), random(255));      // Se rellena la figura con el color original, pero es alterado en el canal R. (P.127) esto hara que palpite el anillo
-    ellipse(0,0,20,20);                     // Se dibujan las figuras que se encuentran en el fondo
-    popMatrix();                                 // Cerrando propiedades independientes
-}
-*/
-
-//como hacer que un circulo vaya creciendo y que dentro vaya creciendo otro círculo
-
-
+public void keyPressed(){
+  if (keyPressed){
+    if (key == 's'){
+    velocidad = 2;
+    }
+  }
 }
 class Tarea{
 
